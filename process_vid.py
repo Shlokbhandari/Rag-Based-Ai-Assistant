538013b6d5473775886f3cf8375b8f29ac72f3ab
Shlokbhandari <shlokbhandari5582@gmail.com>
2026-03-28 11:16:42 +0530

    Changes in codes as data set is changed

--- process_vid.py
+++ process_vid.py
@@ -1,22 +1,10 @@
-import os
+# Converts the videos to mp3 
+import os 
 import subprocess
 
-input_folder = "Videos"
-output_folder = "Audios"
-
-files = [f for f in os.listdir(input_folder) if f.endswith(".mp4") and not f.startswith(".")]
-
-for file in files:
-    name_without_ext = file.rsplit(".", 1)[0]
-    tutorial_number, file_name = name_without_ext.split("_", 1)
-
-    input_path = os.path.join(input_folder, file)
-    output_path = os.path.join(output_folder, f"{tutorial_number}_{file_name}.mp3")
-
-    subprocess.run([
-        "ffmpeg",
-        "-i", input_path,
-        "-vn",
-        "-b:a", "192k",
-        output_path
-    ])
+files = os.listdir("videos") 
+for file in files: 
+    tutorial_number = file.split(" [")[0].split(" #")[1][:-4]
+    file_name = file.split(" | ")[0]
+    print( tutorial_number,  file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
