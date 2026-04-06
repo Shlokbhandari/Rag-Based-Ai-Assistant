48fd11885e9cf2339f75e955dc9545bca95d067a
Shlokbhandari <shlokbhandari5582@gmail.com>
2026-04-06 12:05:12 +0530

    Making prompt to give to llm

--- process_incoming.py
+++ process_incoming.py
@@ -15,6 +15,12 @@ def create_embedding(text_list):
     embedding = r.json()["embeddings"]
     return embedding
 
+def inference(prompt):
+    r =requests.post("http://localhost:11434/api/generate", json={
+        "model": "deepseek-r1", 
+        "prompt": prompt,
+        "stream": False
+    })
 
 df = joblib.load("embeddings.joblib")
 input_query = input("Ask a question: ")
@@ -25,9 +31,18 @@ question_embedding = create_embedding([input_query])[0]
 # print(np.vstack(df["embedding"]).shape) #Cosine similarity funtion onyl takes 2d array as input 
 similarities = cosine_similarity(np.vstack(df["embedding"]), [question_embedding]).flatten()
 
-top_results = 30
+top_results = 15
 max_indx = similarities.argsort()[::-1][0:top_results]
 new_df = df.loc[max_indx]
 
-for index, item in new_df.iterrows():
-    print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
+prompt = f'''
+I am teaching web development using Sigma Web Development couse. Here are vioe chunks with video title, video number, start seconds of the chunk. end seconds of the chunk , and the text at that time:
+
+{new_df[["title", "number", "start", "end", "text"]].to_json()}
+------------------------------------------------
+{input_query}
+User aked this question related to video chunks, you have to answer where and how much content is taught in which video (in which video and what timstamp) and guide the user to go to that particular video. If user asks unrelated questions, tell him that you can only answer questions related to the course.
+'''
+
+# for index, item in new_df.iterrows():
+#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
